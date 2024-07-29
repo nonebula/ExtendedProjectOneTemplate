86ab576d3dfb2d5f618c825b980e3378cb36bbc6
Joe Higgs <joe.higgs@mercator.group>
2024-07-29 15:46:41 +0100

    Add new controller and setup test file

---
+++ app/controllers/ApplicationController.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import play.api.mvc.{BaseController, ControllerComponents}
+
+import javax.inject.{Inject, Singleton}
+
+
+@Singleton
+class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  def index() = TODO
+
+  def create() = TODO
+
+  def read(id: String) = TODO
+
+  def update(id: String) = TODO
+
+  def delete(id: String) = TODO
+}
---
+++ test/controllers/ApplicationControllerSpec.scala
@@ -0,0 +1,11 @@
+package controllers
+
+import baseSpec.BaseSpecWithApplication
+
+class ApplicationControllerSpec extends BaseSpecWithApplication {
+
+  val TestApplicationController = new ApplicationController(
+    component
+  )
+  
+}
