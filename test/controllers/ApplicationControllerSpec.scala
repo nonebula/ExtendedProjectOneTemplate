d2a9447bba2e5d6e29ef2bd7f3d2168c545a59c0
Joe Higgs <joe.higgs@mercator.group>
2024-07-29 15:48:59 +0100

    Create placeholder tests

--- test/controllers/ApplicationControllerSpec.scala
+++ test/controllers/ApplicationControllerSpec.scala
@@ -7,5 +7,24 @@ class ApplicationControllerSpec extends BaseSpecWithApplication {
   val TestApplicationController = new ApplicationController(
     component
   )
-  
+
+  "ApplicationController .index()" should {
+
+  }
+
+  "ApplicationController .create()" should {
+
+  }
+
+  "ApplicationController .read()" should {
+
+  }
+
+  "ApplicationController .update()" should {
+
+  }
+
+  "ApplicationController .delete()" should {
+
+  }
 }
