2e07cd1e36fefbeaf2175a88b61f236596bf9fa6
Joe Higgs <joe.higgs@mercator.group>
2024-07-31 17:07:46 +0100

    Setup self for completing case class and google book method

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -5,13 +5,14 @@ import models.DataModel
 import play.api.libs.json.{JsError, JsSuccess, JsValue, Json}
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, Result}
 import repositories.DataRepository
+import services.LibraryService
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
 
 @Singleton
-class ApplicationController @Inject()(val dataRepository: DataRepository, val controllerComponents: ControllerComponents)(implicit val ec: ExecutionContext) extends BaseController {
+class ApplicationController @Inject()(val dataRepository: DataRepository, val controllerComponents: ControllerComponents, libraryService: LibraryService)(implicit val ec: ExecutionContext) extends BaseController {
 
   def index(): Action[AnyContent] = Action.async { implicit request =>
     dataRepository.index().map {
@@ -54,6 +55,14 @@ class ApplicationController @Inject()(val dataRepository: DataRepository, val co
       }
     }.recover {
       case ex: Exception => InternalServerError(Json.toJson("Error deleting item"))
+    }
+  }
+
+  //Add the corresponding method to your controller, it's common practice naming the controller method the same as/similar to the service method
+  // Fill in the missing implementation to return a Ok response, along with a Json body containing the book we found.
+  def getGoogleBook(search: String, term: String): Action[AnyContent] = Action.async { implicit request =>
+    libraryService.getGoogleBook(search = search, term = term).map {
+      ???
     }
   }
 }
